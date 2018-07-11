fc461c7d4bbefa80eb7f095a1605a986542edbad
Ary Y <gtownx36@gmail.com>
2018-07-11 13:22:13 +0530

    Working update pogchamp

--- poeRPC.py
+++ poeRPC.py
@@ -3,9 +3,12 @@ import aiohttp
 import rpc
 import time
 import hashlib
+import time
+import json
 
 from enum import Enum
 from getdir import get_path
+from pathlib import Path
 
 CLIENT_ID = '466251900157820929'
 
@@ -28,6 +31,14 @@ class PoeRPC:
         self.afk_message = ""
         self.account_name = account_name
         self.current_rpc = {}
+        self.locations = {}
+        with open('areas.json') as f:
+            areas = json.load(f)
+        self.locations.update(areas)
+        self.locations['guardians'] = ['forge of the phoenix', 'maze of the minotaur', 'lair of the hydra', 'pit of the chimera']
+        with open('maps.json') as f:
+            self.maps = json.load(f)
+        
 
     async def check_poe(self):
         """Try to check if poe is running every 15sec, disable RPC if not.
@@ -43,7 +54,10 @@ class PoeRPC:
 
     def update_rpc(self, field, value):
         self.current_rpc[field] = value
-
+    def update_subdict(self, subdict, field, value):
+        if subdict not in self.current_rpc.keys():
+            self.current_rpc[subdict] = {}
+        self.current_rpc[subdict][field] = value
     def submit_update(self):
         self.rpc.set_activity(self.current_rpc)
 
@@ -58,11 +72,67 @@ class PoeRPC:
         name = char['name']
         details = f"Level {level} {asc} {name}"
         self.update_rpc('details', details)
-        self.update_rpc('large_image', asc.lower())
+        self.update_subdict('assets', 'large_image', asc.lower())
+        self.update_subdict('assets', 'large_text', details)
 
-    async def get_area_data(self, name):
-        pass
+    @staticmethod
+    def fix_names(self, name):
+        return name.replace(',', '').replace("'", "").replace('ö', 'o').replace(' ', '_').lower()
 
+    async def fetch_area_data(self, name):
+        loc = None
+        if 'hideout' in name.lower():
+            loc = {'name': name}
+            img = 'hideout'
+        if not loc:
+            for map in self.maps:
+                if name in map['name']:
+                    loc = map
+                    fixed_name = self.fix_names(name)
+                    p = Path.cwd().joinpath(f'assets/{fixed_name}.png')
+                    if p.is_file():
+                        img = fixed_name
+                    elif int(map['tier']) <6:
+                        img = 'white'
+                    elif 6 <= int(map['tier']) <=10:
+                        img = 'yellow'
+                    elif int(map['tier']) > 10:
+                        img = 'red'
+                    elif name.lower() in self.locations['guardians']:
+                        if 'Phoenix' in name:
+                            img = 'phoenix'
+                        elif 'Minotaur' in name:
+                            img = 'minotaur'
+                        elif 'Hydra' in name:
+                            img = 'hydra'
+                        else:
+                            img = 'chimera'
+                    elif name in "Vaal Temple":
+                        img = 'vaal_temple'
+                    break
+        if not loc:
+            for town in self.locations['towns']:
+                if name in town['name']:
+                    loc = town
+                    img = 'town'
+                    break
+        if not loc:
+            for part in self.locations['lab_rooms']:
+                if part in name:
+                    loc = {'name': "The Lord's Labyrinth"}
+                    img = 'lab'
+                    break
+        if not loc:
+            loc = {'name': name}
+            img = 'waypoint'
+        small_text = loc['name']
+        timestamp = round(time.time())
+        self.update_subdict('assets', 'small_text', small_text)
+        self.update_subdict('assets', 'small_image', img)
+        self.update_subdict('timestamps', 'start', timestamp)
+        self.update_rpc('state', f'in {name}')
+        if 'tier' in loc.keys():
+            self.update_rpc('details', f"Tier: {loc['tier']}")
     async def handle_log_event(self, log):
         messages = reversed(log.split('\n'))
         event = None
@@ -73,7 +143,7 @@ class PoeRPC:
             elif "You have entered" in message:
                 loc = message.split("You have entered ")[1].replace('.','')
                 print("entered", loc)
-                if self.last_location != loc:
+                if self.last_location != loc and loc != "Arena":
                     event = LogEvents.AREA
                     self.last_location = loc
                 else:
@@ -99,6 +169,12 @@ class PoeRPC:
                 else:
                     self.dnd = False
         self.last_latest_message = log.split('\n')[-1] or log.split('\n')[-2]
+        if self.last_event == LogEvents.LOGOUT:
+            await self.fetch_char()
+        if event == LogEvents.AREA:
+            await self.fetch_area_data(loc)
+        self.submit_update()
+        self.last_event = event
     async def monitor_log(self):
         """Monitor if log file has changed by calculating md5 hash,
         pass log to handler if yes, try again in 5 seconds"""
@@ -116,20 +192,19 @@ class PoeRPC:
                 await self.handle_log_event(log)
             await asyncio.sleep(5)
     @staticmethod
-    def get_poe():
+    async def get_poe():
         """When initializing, keep trying to find a launched poe before we can init here"""
-        # while 1:
-            # poe = get_path()
-            # if not poe:
-                # time.sleep(10)
-            # else:
-                # return poe
-        return "I:/SteamLibrar/steamapps/common/Path of Exile"
+        while 1:
+            poe = get_path()
+            if not poe:
+                await asyncio.sleep(10)
+            else:
+                return poe
+                #return "I:/SteamLibrar/steamapps/common/Path of Exile"
     async def init(self):
         print('init')
-        poe = self.get_poe()
+        poe = await self.get_poe()
         self.log_path = f"{poe}/logs/Client.txt"
         self.ses = aiohttp.ClientSession()
-        #self.loop.ensure_future(check_poe())
-        await asyncio.sleep(2)
+        self.loop.create_task(self.check_poe())
         self.loop.create_task(self.monitor_log())
