37c2022e114b0068d6e08ac6bc5fd074fcbbf006
Ary Y <gtownx36@gmail.com>
2018-07-11 20:34:03 +0530

    use config.json for account name

--- launcher.py
+++ launcher.py
@@ -1,7 +1,13 @@
 import asyncio
+import json
+
 from poeRPC import PoeRPC
+
+with open('config.json') as f:
+    js = json.load(f)
+
 loop = asyncio.get_event_loop()
-cl = PoeRPC(loop, 'xKynn')
+cl = PoeRPC(loop, js['name'])
 #print(dir(loop))
 loop.run_until_complete(cl.init())
 loop.run_forever()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="PathOfExileRPC",
+    version="",
+    description='',
+    executables=[Executable("launcher.py")],
+    options={
+        'build_exe': {
+            'packages': ['aiohttp', 'idna', 'asyncio']
+        },
+    }
+)
